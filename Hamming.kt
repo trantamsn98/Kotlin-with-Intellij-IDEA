bf597bfc11529c36520d8dbc7f8caf50f2a2b4d4
trantamsn98 <63984222+trantamsn98@users.noreply.github.com>
2020-04-23 18:36:39 +0700

    Update Hamming.kt

--- Hamming.kt
+++ Hamming.kt
@@ -1,15 +1,22 @@
-fun Hamming(Str1:String, Str2:String):Int{
-    var Distance:Int = 0
-    for(i in 0 until Str1.length){
-        if(Str1[i].equals(Str2[i]))
-        else Distance++
+object Hamming {
+
+    fun compute(Str1:String, Str2:String): Int {
+        var Distance:Int = 0
+        require(Str1.length == Str2.length) {
+            for (i in 0 until Str1.length) {
+                if (Str1[i].equals(Str2[i]))
+                else Distance++
+            }
+        }
+        return Distance
     }
-    return Distance
+
+
+//    fun main() {
+//        var DNA1: String = "GAGCCTACTAACGGGAT"
+//        var DNA2: String = "CATCGTAATGACGGCCT"
+//        var Result: Int = compute(DNA1, DNA2)
+//        println(Result)
+//    }
 }
 
-fun main(){
-    var DNA1:String = "GAGCCTACTAACGGGAT"
-    var DNA2:String = "CATCGTAATGACGGCCT"
-    var Result:Int = Hamming(DNA1, DNA2)
-    println(Result)
-}
